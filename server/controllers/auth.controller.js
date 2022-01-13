5fd4c6af70656a75663c5fa44ff58262e19a2c4a
Thomas Melchers <thomas.melchers@gmail.com>
2022-01-13 11:30:20 +0100

    modif authentification

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -25,18 +25,16 @@ module.exports.signUp = async (req, res) => {
   }
 
   module.exports.signIn = async (req, res) => {
-    const {email, password } = req.body
+    const { email, password } = req.body
 
     try {
       const user = await UserModel.login(email, password)
-      const token = createToken (user.id)
-      //httpOnly is used for security
-      res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge})
-      res.status(200).json({user: user._id})
-    }
-    catch(err){
-      const errors = signInErrors(err);
-      res.status(200).json({errors})
+      const token = createToken(user._id)
+      res.cookie('jwt', token, { httpOnly: true, maxAge })
+      res.status(200).json({ user: user._id })
+    } catch (err) {
+      const errors = signInErrors(err)
+      res.status(200).json({ errors })
     }
   }
 
